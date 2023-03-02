eb5cc0f2909ebe34a141e9bc0772ab6fe692a3d8
zhouyu88888 <844415622@qq.com>
2023-03-02 09:44:52 +0800

    0302

--- TXTraintickets/testcases/1、启动铁行火车票/启动应用.py
+++ TXTraintickets/testcases/1、启动铁行火车票/启动应用.py
@@ -9,12 +9,13 @@
 from appium import webdriver
 from appium.webdriver.common.appiumby import AppiumBy
 from time import sleep
+import os
 
 # For W3C actions
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.actions import interaction
-from selenium.webdriver.common.actions.action_builder import ActionBuilder
-from selenium.webdriver.common.actions.pointer_input import PointerInput
+# cmd命令清缓存进app
+# 'r' 消除转义符带来的影响,即'\'
+retValue = os.popen('adb shell pm clear com.tiexing', 'r')
+print(retValue)
 
 caps = {}
 caps["platformName"] = "Android"
